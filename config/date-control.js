16a70c02e8f9eff64a478944e3fa24138f99757c
toaykotobuki-shikahama <toaykotobuki@gmail.com>
2026-07-28 19:32:41 +0900

    Update date-control.js

--- config/date-control.js
+++ config/date-control.js
@@ -1,7 +1,7 @@
 window.TREASURE_DATE_CONTROL = {
   // "live": 日本時間の実際の日付を参照
   // "test": 下の testDate を参照
-  mode: "live",
+  mode: "test",
 
   // テストしたい開催日を YYYY-MM-DD で指定
   testDate: "2026-08-10"
